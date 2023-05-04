f649a5569a10c68c95f5fbfa64e99202b51f02f6
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2023-05-04 00:00:46 -0300

    feat: view Home

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,7 +1,39 @@
 <template>
-  <h1>Home</h1>
+  <Header />
+  <main>
+    <h2 class="h2-title">Camisetas</h2>
+
+    <div class="product-grid">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  </main>
 </template>
 
-<script></script>
+<script>
+import Header from "../../components/Header/Header.vue";
+import ProductCard from "../../components/ProductCard/ProductCard.vue";
+
+export default {
+  name: "Home",
+  components: { Header, ProductCard },
+};
+</script>
 
 <style lang="less" src="./style.less" scoped />
