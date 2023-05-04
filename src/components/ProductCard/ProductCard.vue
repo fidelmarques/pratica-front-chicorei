75ec7cdf00d473511c40a06d592fd07ecbf1b46a
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2023-05-04 00:01:37 -0300

    feat: componente ProductCard

---
+++ src/components/ProductCard/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product-card">
+    <img
+      class="product-image"
+      src="../../assets/e123fc40-decc-11ed-b748-115d167b6c8d.avif"
+    />
+    <div class="product-info">
+      <h3 class="product-title">Camiseta Clean</h3>
+      <h5 class="product-description">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </h5>
+      <h4 class="product-price">R$ 39,99</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+};
+</script>
+
+<style lang="less" src="./style.less" scoped />
