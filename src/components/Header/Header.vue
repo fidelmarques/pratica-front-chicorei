0a11a3afdc4f7e37cb4c2c9a378857625c8ae3bd
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2023-05-07 23:33:41 -0300

    feat: adição do menu slider

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,7 +2,7 @@
   <header id="header">
     <nav class="navbar navbar-expand-lg">
       <div class="container-fluid">
-        <button type="button" class="btn menu-button">
+        <button type="button" class="menu-button" @click="toggleMenu">
           <i class="bi bi-list"></i>
         </button>
 
@@ -10,17 +10,44 @@
           <h1 class="header-title">Chico Rei</h1>
         </a>
 
-        <button type="button" class="btn cart-button">
+        <button type="button" class="cart-button">
           <i class="bi bi-bag"></i>
         </button>
       </div>
     </nav>
+    <Menu v-if="collapsed" :class="{ close: closing }" />
   </header>
 </template>
 
 <script>
+import { ref } from "vue";
+import { blurred } from "../../views/Home/Home.vue";
+
+import Menu from "../Menu/Menu.vue";
+
+export const collapsed = ref(false);
+export const closing = ref(false);
+
+export const toggleMenu = () => {
+  if (collapsed.value) {
+    closing.value = true;
+    setTimeout(() => {
+      collapsed.value = !collapsed.value;
+      closing.value = false;
+    }, 300);
+  } else {
+    collapsed.value = !collapsed.value;
+  }
+  blurred.value = !blurred.value;
+};
+
 export default {
   name: "Header",
+  components: { Menu },
+
+  setup() {
+    return { collapsed, blurred, closing, toggleMenu };
+  },
 };
 </script>
 
---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="menu-container">
+    <div class="menu">
+      <div class="menu-header">
+        <span>Menu</span>
+        <button type="button" class="menu-button" @click="toggleMenu">
+          <i class="bi bi-x"></i>
+        </button>
+      </div>
+
+      <ul class="menu-list">
+        <li class="menu-item">
+          <a class="" href="#">Página inicial</a>
+        </li>
+        <li class="menu-item">
+          <a class="" href="#">Camisetas</a>
+        </li>
+        <li class="menu-item">
+          <a class="" href="#">Moletons</a>
+        </li>
+        <li class="menu-item">
+          <a class="" href="#">Shorts</a>
+        </li>
+        <li class="menu-item">
+          <a class="" href="#">Meias</a>
+        </li>
+        <li class="menu-item">
+          <a class="" href="#">Bermudas</a>
+        </li>
+        <li class="menu-item">
+          <a class="" href="#">Calças</a>
+        </li>
+      </ul>
+    </div>
+    <a class="menu-blurred-background" @click="toggleMenu"></a>
+  </div>
+</template>
+
+<script>
+import { toggleMenu } from "../Header/Header.vue";
+export default {
+  name: "Menu",
+  setup() {
+    return { toggleMenu };
+  },
+};
+</script>
+
+<style lang="less" src="./style.less" scoped />
